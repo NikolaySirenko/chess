2a8d278c12fea50f0a791363daa20ada4a265760
Nikolay Favorskiy <lordnikolas9091@gmail.com>
2025-02-07 23:08:58 +0100

    printboard

--- src/ConsoleChess.java
+++ src/ConsoleChess.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+// CREATIONAL: Factory Method
 // Class of a piece
 abstract class Piece {
     // piece color
@@ -10,6 +11,12 @@ abstract class Piece {
     }
     // abstract declaration of method checking validity of a move for the piece
     abstract boolean isValidMove(int x1, int y1, int x2, int y2);
+
+    public String shortName() {
+        return ((color.equals("White") ? "W" : "B") +
+                (this instanceof Knight ? "N" : this.getClass().getSimpleName().charAt(0))
+        );
+    }
 }
 
 // Class of a pawn - extends piece
@@ -22,12 +29,67 @@ class Pawn extends Piece {
     }
 }
 
+// Class of a rook - extends piece
+class Rook extends Piece {
+    public Rook(String color) { super(color); }
+    // override of method checking validity of a move for the rook
+    @Override
+    boolean isValidMove(int x1, int y1, int x2, int y2) {
+        return x1 == x2 || y1 == y2;
+    }
+}
+
+// Class of a knight - extends piece
+class Knight extends Piece {
+    public Knight(String color) { super(color); }
+    // override of method checking validity of a move for the knight
+    @Override
+    boolean isValidMove(int x1, int y1, int x2, int y2) {
+        return (Math.abs(x1 - x2) == 2 && Math.abs(y1 - y2) == 1) || (Math.abs(x1 - x2) == 1 && Math.abs(y1 - y2) == 2);
+    }
+}
+
+// Class of a bishop - extends piece
+class Bishop extends Piece {
+    public Bishop(String color) { super(color); }
+    // override of method checking validity of a move for the bishop
+    @Override
+    boolean isValidMove(int x1, int y1, int x2, int y2) {
+        return Math.abs(x1 - x2) == Math.abs(y1 - y2);
+    }
+}
+
+// Class of a queen - extends piece
+class Queen extends Piece {
+    public Queen(String color) { super(color); }
+    // override of method checking validity of a move for the queen
+    @Override
+    boolean isValidMove(int x1, int y1, int x2, int y2) {
+        return x1 == x2 || y1 == y2 || Math.abs(x1 - x2) == Math.abs(y1 - y2);
+    }
+}
+
+// Class of a king - extends piece
+class King extends Piece {
+    public King(String color) { super(color); }
+    // override of method checking validity of a move for the king
+    @Override
+    boolean isValidMove(int x1, int y1, int x2, int y2) {
+        return Math.abs(x1 - x2) <= 1 && Math.abs(y1 - y2) <= 1;
+    }
+}
+
 // CREATIONAL: Factory Method
 // Factory class for creating new pieces
 class PieceFactory {
     public static Piece createPiece(String type, String color) {
         return switch (type) {
             case "Pawn" -> new Pawn(color);
+            case "Rook" -> new Rook(color);
+            case "Knight" -> new Knight(color);
+            case "Bishop" -> new Bishop(color);
+            case "Queen" -> new Queen(color);
+            case "King" -> new King(color);
             default -> throw new IllegalArgumentException("Invalid piece type");
         };
     }
@@ -42,6 +104,80 @@ class ChessBoard {
     private ChessBoard() {
         board = new Piece[8][8];
     }
+    // method for moving a piece over the board
+    public boolean movePiece(int x1, int y1, int x2, int y2) {
+        // return false if move is invalid
+        if (board[x1][y1] == null || !board[x1][y1].isValidMove(x1, y1, x2, y2)) {
+            return false;
+        }
+        // copy piece from point a to point b
+        board[x2][y2] = board[x1][y1];
+        // remove piece from point a
+        board[x1][y1] = null;
+        return true;
+    }
+
+    // Method for printing the chessboard to console
+    public void printBoard() {
+        System.out.println("     a      b      c      d      e      f      g      h ");
+        System.out.println("  +------+------+------+------+------+------+------+------+");
+        for (int y = 7; y >= 0; y--) {
+
+            System.out.print("  |");
+            for (int x = 0; x < 8; x++) {
+                System.out.print(((x + y) % 2 == 0 ? "      " : "░░░░░░") + "|");
+            }
+            System.out.println();
+
+            System.out.print((y + 1) + " |");
+            for (int x = 0; x < 8; x++) {
+                if((x + y) % 2 == 0){
+                    System.out.print((board[x][y] == null ? "      " : "  " + board[x][y].shortName() + "  ") + "|");
+                }
+                else{
+                    System.out.print((board[x][y] == null ? "░░░░░░" : "░░" + board[x][y].shortName() + "░░") + "|");
+                }
+            }
+            System.out.println(" " + (y + 1));
+
+            System.out.print("  |");
+            for (int x = 0; x < 8; x++) {
+                System.out.print(((x + y) % 2 == 0 ? "      " : "░░░░░░") + "|");
+            }
+            System.out.println();
+
+            System.out.println("  +------+------+------+------+------+------+------+------+");
+        }
+        System.out.println("     a      b      c      d      e      f      g      h ");
+    }
+
+    // method for checking if a players king is in check
+    public boolean isKingInCheck(String color) {
+        // Algorithm for finding player's king coordinates
+        int kingX = -1, kingY = -1;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (board[i][j] instanceof King && board[i][j].color.equals(color)) {
+                    kingX = i;
+                    kingY = j;
+                }
+            }
+        }
+        // return false if king is absent
+        if (kingX == -1) return false;
+        // iterate over the chessboard
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                // return true if there is an enemy piece attacking king's coordinates
+                if (board[i][j] != null && !board[i][j].color.equals(color) && board[i][j].isValidMove(i, j, kingX, kingY)) {
+                    return true;
+                }
+            }
+        }
+        // return false by default
+        return false;
+    }
+
     // Builder class of a chessboard builder for a new chessboard initialisation
     public static class Builder {
         private ChessBoard chessBoard;
@@ -49,9 +185,40 @@ class ChessBoard {
         public Builder() {
             chessBoard = new ChessBoard();
         }
+
         // method for adding a new piece to the board with specified type, color and coordinates
         public Builder addPiece(String type, String color, int x, int y) {
             chessBoard.board[x][y] = PieceFactory.createPiece(type, color);
+            return this;
+        }
+
+        public Builder setupStandardBoard() {
+            // Place pawns
+            for (int i = 0; i < 8; i++) {
+                this.addPiece("Pawn", "White", i, 1);
+                this.addPiece("Pawn", "Black", i, 6);
+            }
+            // Place rooks
+            this.addPiece("Rook", "White", 0, 0);
+            this.addPiece("Rook", "White", 7, 0);
+            this.addPiece("Rook", "Black", 0, 7);
+            this.addPiece("Rook", "Black", 7, 7);
+            // Place knights
+            this.addPiece("Knight", "White", 1, 0);
+            this.addPiece("Knight", "White", 6, 0);
+            this.addPiece("Knight", "Black", 1, 7);
+            this.addPiece("Knight", "Black", 6, 7);
+            // Place bishops
+            this.addPiece("Bishop", "White", 2, 0);
+            this.addPiece("Bishop", "White", 5, 0);
+            this.addPiece("Bishop", "Black", 2, 7);
+            this.addPiece("Bishop", "Black", 5, 7);
+            // Place queens
+            this.addPiece("Queen", "White", 3, 0);
+            this.addPiece("Queen", "Black", 3, 7);
+            // Place kings
+            this.addPiece("King", "White", 4, 0);
+            this.addPiece("King", "Black", 4, 7);
             return this;
         }
 
@@ -111,33 +278,59 @@ class GameLogger implements Observer {
 
 // Class of a chessgame
 class ChessGame {
+    private ChessBoard board;
+    // Current turn
+    private String turn;
+
+    // ChessGame constructor
+    public ChessGame() {
+        // create a new chessboard with starting pieces using the builder
+        board = new ChessBoard.Builder().setupStandardBoard().build();
+        // set current turn to white
+        turn = "White";
+    }
+
     // List of observers for logging
     private List<Observer> observers = new ArrayList<>();
     // Adding console logging to the list of observers
     public void addObserver(Observer observer) {
         observers.add(observer);
     }
+
+    public void printBoard() {
+        board.printBoard();
+    }
+
     // Notify all observers in the list with the made move
-    public void makeMove(String move) {
-        for (Observer observer : observers) {
-            observer.update(move);
+    public boolean makeMove(int x1, int y1, int x2, int y2) {
+        if (board.movePiece(x1, y1, x2, y2)) {
+            turn = turn.equals("White") ? "Black" : "White";
+            if (board.isKingInCheck(turn)) {
+                System.out.println(turn + " King is in check!");
+            }
+            for (Observer observer : observers) {
+                observer.update(String.format("Move: %d%d %d%d", x1, y1, x2, y2));
+            }
+            return true;
         }
+        return false;
     }
 }
 
 public class ConsoleChess {
     public static void main(String[] args) {
-        // Building a new chessboard
-        ChessBoard chessBoard = new ChessBoard.Builder()
-                .addPiece("Pawn", "White", 1, 1)
-                .addPiece("Pawn", "Black", 6, 6)
-                .build();
 
         // Initialising a new chessgame
         ChessGame game = new ChessGame();
         // adding a console logger to the chessgame
         game.addObserver(new GameLogger());
-        // making a move with white
-        game.makeMove("Pawn from (1,1) to (1,2)");
+
+        game.printBoard();
+        game.makeMove(1, 1, 1, 2);
+        game.printBoard();
+
+        game.makeMove(0, 0, 0, 5);
+        game.printBoard();
+        game.makeMove(0, 1, 2, 2);
     }
 }
